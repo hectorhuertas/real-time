4fd9dfd9517f817b4f051f80430fde5b93a8e11c
Hector Huertas <hectorhuertas@gmail.com>
2016-04-20 22:40:22 +0200

    Save polls to memory

--- server.js
+++ server.js
@@ -20,17 +20,37 @@ const server = http.createServer(app) .listen(port, function() {
 const socketIo = require('socket.io');
 const io = socketIo(server);
 const generateSecret = require('./lib/generate-secret');
+
+const polls = {};
+  polls.prueba = {
+    secret: 'sagds3w3wt',
+    ['opt 1']: 0,
+    ['opt 2']: 0,
+    ['opt 3']: 0
+  };
 io.on('connection', function(socket){
   console.log('someone connected');
 
   socket.on('message', function (channel, msg) {
     if (channel === 'newPoll') {
-      var newLinks = {
-        admin: host + msg.title + '-' + generateSecret() + '.html',
+      const secret = generateSecret();
+      addPoll(msg, secret);
+      const newLinks = {
+        admin: host + msg.title + '-' + secret + '.html',
         voting: host + 'admin.html'
       };
       socket.emit('newLinks', newLinks);
     }
     console.log(msg);
+    console.log(polls);
   });
 });
+
+function addPoll(poll, secret){
+  polls[poll.title] = {
+    secret: secret,
+    [poll.one]: 0,
+    [poll.two]: 0,
+    [poll.three]: 0
+  };
+}
