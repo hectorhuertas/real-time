a72ce176e970842116e88dba33d27539e5c10279
Hector Huertas <hectorhuertas@gmail.com>
2016-04-20 18:53:18 +0200

    Setup simplest server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+app.use(express.static('public'));
+
+// ROUTES
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html' );
+});
+
+// ROUTES END
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app) .listen(port, function() {
+  console.log('Listening on port ' + port);
+});
