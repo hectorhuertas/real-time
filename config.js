d3f31f01c042bd622c808647d7d5e21a049c94e0
Hector Huertas <hectorhuertas@gmail.com>
2016-04-21 19:32:32 +0200

    Delete poll fixture

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: process.env.NODE_ENV === 'production' ? 'https://real-time-hector.herokuapp.com/' : 'http://localhost:3000/',
-  set: function(app){
+  set: function(app, express){
     app.use(express.static('public'));
     app.set('view engine', 'ejs');
   },
--- server.js
+++ server.js
@@ -1,45 +1,14 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+
 const config = require('./config.js');
+config.set(app, express);
 
-config.set(app);
-// app.use(express.static('public'));
-// app.set('view engine', 'ejs');
 const polls = {};
-polls['test-poll'] = {
-  id: 'test-poll',
-  title: 'Test Poll',
-  secret: 'secret',
-  options: {
-    ['Yeah!']: 0,
-    ['So so']: 0,
-    ['No way']: 0
-  }
-};
-// ROUTES
+
 require('./routes.js')(app, polls);
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/public/index.html' );
-// });
 
-// app.get('/polls/:id', function(req, res){
-//   res.render('voting', {poll: polls[req.params.id]});
-//   console.log(polls);
-// });
-//
-// app.get('/polls/:id/admin/:secret', function(req, res){
-//   const owner = req.params.secret === polls[req.params.id].secret;
-//   if (owner) {
-//     res.render('admin', {poll: polls[req.params.id]});
-//   } else {
-//     res.sendStatus(404);
-//   }
-//   console.log(polls);
-// });
-
-// ROUTES END
-// const host = process.env.NODE_ENV === 'production' ? 'https://real-time-hector.herokuapp.com/' : 'http://localhost:3000/';
 const host = config.host;
 const port = process.env.PORT || 3000;
 const server = http.createServer(app) .listen(port, function() {
