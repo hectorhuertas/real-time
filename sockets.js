49d6e136025da9488bb010abde239ad50a7ae2ad
Hector Huertas <hectorhuertas@gmail.com>
2016-04-23 22:19:20 +0200

    Extract sending responses to sockets

--- sockets.js
+++ sockets.js
@@ -1,5 +1,19 @@
 const pollManager = require('./lib/poll-manager');
 
+function send(responses, io, socket){
+  for (var i = 0; i < responses.length; i++) {
+    // console.log(responses[i]);
+    switch (responses[i].type) {
+      case 'one': socket.emit(responses[i].channel, responses[i].msg);break;
+      case 'two': console.log('yeah');
+      // console.log('bob');
+        break;
+      default:
+
+    }
+  }
+}
+
 function sockets(io, config){
   const polls = require('./lib/polls');
 
@@ -8,10 +22,14 @@ function sockets(io, config){
 
     socket.on('message', function (channel, msg) {
       pollManager.closeOutdated(io);
-
-      if (channel === 'newPoll')   { pollManager.create(msg, socket); }
+      const responses = [];
+      if (channel === 'newPoll')   {
+        responses.push(pollManager.create(msg, socket));
+      }
       if (channel === 'newVote')   { pollManager.newVote(msg, io, socket); }
       if (channel === 'closePoll') { pollManager.closePoll(msg, io); }
+      // console.log(responses);
+      send(responses, io, socket);
     });
   });
 }
