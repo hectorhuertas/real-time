66f1477cc1b2364b776de9035770c71145961d1f
Hector Huertas <hectorhuertas@gmail.com>
2016-04-23 20:34:45 +0200

    Move poll creation to pollManager

--- sockets.js
+++ sockets.js
@@ -1,9 +1,7 @@
-
-const moment = require('moment');
 const pollManager = require('./lib/poll-manager');
+
 function sockets(io, config){
   const polls = require('./lib/polls');
-  const generateSecret = require('./lib/generate-secret');
 
   io.on('connection', function(socket){
     console.log('Someone connected');
@@ -11,41 +9,11 @@ function sockets(io, config){
     socket.on('message', function (channel, msg) {
       pollManager.closeOutdated(io);
 
-      if (channel === 'newPoll')   { newPoll(socket, msg); }
+      if (channel === 'newPoll')   { pollManager.create(msg, socket); }
       if (channel === 'newVote')   { newVote(socket, msg); }
       if (channel === 'closePoll') { closePoll(io,msg); }
     });
   });
-
-  function addPoll(poll, secret, deadline){
-    polls[slugify(poll.title)] = {
-      id: slugify(poll.title),
-      title: poll.title,
-      secret: secret,
-      deadline: poll.deadline,
-      options: {
-        [poll.one]: 0,
-        [poll.two]: 0,
-        [poll.three]: 0
-      }
-    };
-  }
-
-  function slugify(text){
-    return text.toString().toLowerCase().trim()
-      .replace(/&/g, '-and-')
-      .replace(/[\s\W-]+/g, '-');
-  }
-
-  function newPoll(socket, pollData){
-    const secret = generateSecret();
-    addPoll(pollData, secret);
-    const newLinks = {
-      admin: config.host + 'polls/' + slugify(pollData.title) + '/admin/' + secret,
-      voting: config.host + 'polls/' + slugify(pollData.title)
-    };
-    socket.emit('newLinks', newLinks);
-  }
 
   function newVote(socket, msg){
     if (polls[msg.pollId].status !== 'closed') {
