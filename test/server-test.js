bc36bf9ecc1dbddcc94e1727739e2043e1537e41
Hector Huertas <hectorhuertas@gmail.com>
2016-04-24 20:01:29 +0200

    Enhance test

--- test/server-test.js
+++ test/server-test.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const request = require('request');
 const app = require('../server');
+var polls = require('../lib/polls');
+const pollManager = require('../lib/poll-manager');
 
 describe('Server', () => {
 
@@ -15,6 +17,7 @@ describe('Server', () => {
     this.request = request.defaults({
       baseUrl: 'http://localhost:9876/'
     });
+    polls = {};
   });
 
   after(() => {
@@ -41,6 +44,59 @@ describe('Server', () => {
         done();
       });
     });
-  });
 
+    it('should have a body with the poll form', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Title:'));
+        assert(response.body.includes('Options:'));
+        assert(response.body.includes('Add Option'));
+        assert(response.body.includes('Closing time:'));
+        assert(response.body.includes('Start the Poll'));
+        assert(response.body.includes('Poll Links'));
+        done();
+      });
+    });
+
+    it('should have a body with the poll title', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Title:'));
+        done();
+      });
+    });
+
+    it('should have a body with the poll options', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Options:'));
+        assert(response.body.includes('Add Option'));
+        done();
+      });
+    });
+
+    it('should have a body with the poll closing time', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Closing time:'));
+        done();
+      });
+    });
+
+    it('should have a body with the poll submit button', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Start the Poll'));
+        done();
+      });
+    });
+
+    it('should have a body with the poll links', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Poll Links'));
+        done();
+      });
+    });
+  });
 });
