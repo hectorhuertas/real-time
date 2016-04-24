f826c9027781e13260e2afaaab2672e05f904636
Hector Huertas <hectorhuertas@gmail.com>
2016-04-24 18:36:04 +0200

    Setup server test

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ config.set(app, express);
 
 require('./routes.js')(app);
 
-const server = http.createServer(app).listen(config.port, function() {
-  console.log('Listening on port ' + config.port);
-});
+const server = http.createServer(app);
+if (!module.parent) {
+  server.listen(config.port, function() {
+    console.log('Listening on port ' + config.port);
+  });
+}
 
 const socketIo = require('socket.io');
 const io = socketIo(server);
 require('./sockets.js')(io, config);
+
+module.exports = server;
